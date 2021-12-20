f1874890a8a83692f1cfdc348673893deb4c89c5
Lucas Barbato <lucassbarbato96@gmail.com>
2021-12-20 19:12:44 -0300

    para entregar

---
+++ ProyectoTransporte/Proyectotransporte/AppTransporte/AppTransporte/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from AppTransporte import views
+
+urlpatterns = [
+  
+    path('inicio', views.inicio, name="Inicio"),
+    path('pasajero', views.pasajero, name="Pasajero"),
+    path('chofer', views.chofer, name="Chofer"),
+    path('transporte', views.transporte, name="Transporte"),
+    path('terminal', views.terminal, name="Terminal"),
+    path('pasajeroFormulario', views.pasajeroFormulario),
+    path('choferFormulario', views.choferFormulario),
+    path('transporteFormulario', views.transporteFormulario),
+    path('terminalFormulario', views.terminalFormulario),
+]
